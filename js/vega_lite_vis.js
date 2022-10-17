c7136cc98857161924cf346bf3103987b7be34c1
XndyChen <111490969+XndyChen@users.noreply.github.com>
2022-10-17 16:07:21 +1100

    Add files via upload
    done.

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,9 @@
+
+var vg_1 = "js/visualisation.vl.json";
+vegaEmbed('#visualisation', vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "js/bargraph.json";
+vegaEmbed('#bargraph', vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "js/scatterplot.json";
+vegaEmbed('#scatterplot', vg_3).then(function(result) {}).catch(console.error);
